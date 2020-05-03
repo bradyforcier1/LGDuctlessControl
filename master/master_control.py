daf20ff9c5e1be2f9c30b160380117719a98354f
Brady <biosanders@BradyServer>
2020-05-03 17:22:10 -0400

    implemented client

--- master/master_control.py
+++ master/master_control.py
@@ -2,6 +2,7 @@ from random import randint
 from time import strftime
 from flask import Flask, render_template, flash, request, redirect, url_for
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import requests
 
 DEBUG = True
 app = Flask(__name__)
@@ -14,9 +15,11 @@ class Zone(object):
         self.name = name
         self.ip = ip
 
-master_bedroom = Zone('master_bedroom', '123.456')
-guest_bedroom = Zone('guest_bedroom', '123.456')
-living_room = Zone('living_room', '123.456')
+master_bedroom = Zone('master_bedroom', 'http://192.168.1.101:4998')
+guest_bedroom = Zone('guest_bedroom', 'http://192.168.1.101:4998')
+living_room = Zone('living_room', 'http://192.168.1.101:4998')
+
+zones = (master_bedroom, guest_bedroom, living_room)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -27,6 +30,11 @@ def index():
 def temp():
     form = request.form
     temp=request.form['temp']
+
+    for zone in zones:
+      params= {'temp': temp}
+      requests.get(zone.ip+'/temp', params=params )
+
     flash('Set temp to: {}'.format(temp))
     return redirect(url_for('index'))
 
@@ -34,16 +42,27 @@ def temp():
 def sleep():
     form = request.form
     sleep=request.form['sleep']
-    flash('Sleep to sleep in {} minutes'.format(sleep))
+
+    for zone in zones:
+      params= {'sleep': sleep}
+      requests.get(zone.ip+'/sleep', params=params )
+
+    flash('Set to sleep in {} minutes'.format(sleep))
     return redirect(url_for('index'))
 
 @app.route("/off", methods=["GET"])
 def off():
+    for zone in zones:
+      requests.get(zone.ip+'/off')
+
     flash("Turning all zones off")
     return redirect(url_for('index'))
 
 @app.route("/on", methods=["GET"])
 def on():
+    for zone in zones:
+      requests.get(zone.ip+'/off')
+
     flash("Turning all zones on")
     return redirect(url_for('index'))
 

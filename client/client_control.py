81aaddb7db7b26c82732731e5ca27cd8048f00ba
Brady <biosanders@BradyServer>
2020-05-12 19:12:17 -0400

    add client

---
+++ client/client_control.py
@@ -0,0 +1,36 @@
+from random import randint
+from time import strftime
+from flask import Flask, render_template, flash, request, redirect, url_for
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'yeeeeeeee'
+
+
+@app.route("/temp", methods=['GET'])
+def temp():
+    temp=request.args['temp']
+    print ('Set temp to: {}'.format(temp))
+    return '', 200
+
+@app.route("/sleep", methods=['GET'])
+def sleep():
+    sleep=request.args['sleep']
+    print ('Going to sleep in {}'.format(sleep))
+    return '', 200 
+
+@app.route("/off", methods=["GET"])
+def off():
+    print ("Turning off")
+    return '', 200 
+
+@app.route("/on", methods=["GET"])
+def on():
+    print ("Turning on")
+    return '', 200 
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ client/run_client.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=client_control.py
+flask run -h 192.168.1.101 -p 4998
